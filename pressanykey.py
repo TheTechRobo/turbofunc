2ab7aa10f6cda72fed774530563ff2db252ffdcc
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-13 18:06:45 -0400

    fix, better variable name

--- pressanykey.py
+++ pressanykey.py
@@ -1,17 +1,17 @@
 def pressanykey():
     """
     SOURCE: https://raw.githubusercontent.com/TheTechRobo/python-text-calculator/master/FOR%20CLEARING%20THE%20SCREEN%20AND%20PRESS%20ANY%20KEY%20TO%20CONTINUE.md
+    """
     import sys
     try:
         import msvcrt
-        _IS_WINDOWS = True
-        logging.info("Imported msvcrt")
+        windows = True
     except ImportError:
         import tty
         import termios
-        _IS_WINDOWS = False
+        windows = False
     print("Press any key to continue...", end="", flush=True)
-    if _IS_WINDOWS:
+    if windows:
        msvcrt.getch()
     else:
        fd = sys.stdin.fileno()
