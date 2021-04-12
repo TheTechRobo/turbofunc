ff4c95be560e9a1b7c7aeeac6aa6d7389d3a9ce7
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-12 18:10:42 -0400

    Add new parameter to multiprint function
    (Old name of commit: Add new function)

--- turbofunc/multiprint.py
+++ turbofunc/multiprint.py
@@ -1,9 +1,14 @@
-def multiprint(stuffToPrint, *args, **kwargs):
+def multiprint(stuffToPrint, _=None, **kwargs):
     """
     Accepts a dictionary. With key/value pairs text/function.
     For example, you might pass {"Welcome to ": cprint.info, "Palc": cprint.ok, "!" + MANYSPACE: cprint.info}
     which will run cprint.info("Welcome to ");cprint.ok("Palc");cprint.info("!" + MANYSPACE)
-    The *args and **kwargs are given to EVERY function. Useful as an "end=True" or similar.
+    Setting gettext to True will run whateverFunctionYouPassed(_(item), *args, **kwargs) instead of just whateverFunctionYouPassed(item, *args, **kwargs). 
+    The **kwargs is given to EVERY function. Useful as an "end=True" or similar.
     """
+    if _ is None:
+        def _(string):
+            return string
     for item in stuffToPrint:
-        stuffToPrint[item](item, *args, **kwargs)
+        stuffToPrint[item](_(item), **kwargs)
+
