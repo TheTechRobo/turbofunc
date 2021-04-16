c8ea7f0fa5e0059f863f578e40b838e131e113ad
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-16 15:19:17 -0400

    Allow *args

--- turbofunc/multiprint.py
+++ turbofunc/multiprint.py
@@ -1,10 +1,10 @@
-def multiprint(stuffToPrint, _=None, **kwargs):
+def multiprint(stuffToPrint, _=None, *args, **kwargs):
     """
     Accepts a dictionary. With key/value pairs text/function.
     For example, you might pass {"Welcome to ": cprint.info, "Palc": cprint.ok, "!" + MANYSPACE: cprint.info}
     which will run cprint.info("Welcome to ");cprint.ok("Palc");cprint.info("!" + MANYSPACE)
     Setting gettext to True will run whateverFunctionYouPassed(_(item), *args, **kwargs) instead of just whateverFunctionYouPassed(item, *args, **kwargs). 
-    The **kwargs is given to EVERY function. Useful as an "end=True" or similar.
+    The **kwargs and *args are given to EVERY function, *args first. Useful as an "end=True" or similar.
     """
     if _ is None:
         def _(string):
