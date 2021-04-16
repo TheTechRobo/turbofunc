12abca2c60b9662f4154d544961bbb544d791f6c
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-16 15:56:16 -0400

    Sanitise input function

--- turbofunc/__init__.py
+++ turbofunc/__init__.py
@@ -2,6 +2,7 @@ from .clearscreen import *
 from .pressanykey import *
 from .standtextout import *
 from .multiprint import *
+from .sanitiseinput import *
 """
 FUNCTIONS
     clearScreen()
@@ -32,4 +33,12 @@ pressanykey(string='Press any key to continue...', verbose=True)
         Setting gettext to True will run whateverFunctionYouPassed(_(item), *args, **kwargs) instead of just whateverFunctionYouPassed(item, *args, **kwargs). 
         The **kwargs is given to EVERY function. Useful as an "end=True" or similar.
 
+    CleanInput(inp, customRules, appendList=True)
+        Still not perfect, but gets rid of the common culprits (\, ', and zero-width space), and strip()s the text.
+        Then, you can add a list as the second argument (optional) which allows you to add more stuff.
+        Set appendList to False if you ONLY want to use the custom rules.
+        For example, you might run:
+        turbofunc.CleanInput("   This input is NOT CLEAN!sss\", ["sss","!"], appendList=True)
+        That will turn the string passed in from "   This input is NOT CLEAN!sss\" to "This input is NOT CLEAN".
+        Note that the custom rules are done before the defaults.
 """
