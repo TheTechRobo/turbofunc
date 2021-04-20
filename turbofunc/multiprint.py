616c189de452e18a4f05abf028732775386fd86e
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-20 13:43:26 -0400

    Update multiprint.py

--- turbofunc/multiprint.py
+++ turbofunc/multiprint.py
@@ -6,6 +6,7 @@ def multiprint(stuffToPrint, _=None, *args, **kwargs):
     Setting _ to a function will run whateverFunctionYouPassed(_(item), *args, **kwargs) instead of just whateverFunctionYouPassed(item, *args, **kwargs).
     The **kwargs and *args are given to EVERY function, *args first. Useful as an "end=True" or similar.
     Note: To use *args and **kwargs, you MUST write the function like so: multiprint(theDictionary, _=None, *args, **kwargs) instead of multiprint(theDictionary, *args, **kwargs).
+    If using print or cprint, it is **highly recommended** to add flush=True to the end of the function call, because otherwise you may run into issues with the print() cache.
     """
     if _ is None:
         def _(string):
