5df0de7cc9e0c70f943c0274d3e2ddf89bf9fe77
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-20 10:48:45 -0400

    Nobody can see that documentation so im deleting it.

--- turbofunc/__init__.py
+++ turbofunc/__init__.py
@@ -6,13 +6,3 @@ from .sanitiseinput import *
 
 import warnings
 warnings.warn("Importing turbofunc by itself is deprecated and not recommended. It will be removed in turbofunc 2.0. Run help(turbofunc) to see the possible imports.", DeprecationWarning, stacklevel=2) #https://stackoverflow.com/a/30093619/9654083
-"""
-    For documentation, please import the corresponding module first and then run help() on IT.
-    There are:
-      - turbofunc.clearscreen
-      - turbofunc.pressanykey
-      - turbofunc.standtextout
-      - turbofunc.multiprint
-      - turbofunc.sanitiseinput
-    This __init__.py is ONLY still provided for backwards compatibility.
-"""
